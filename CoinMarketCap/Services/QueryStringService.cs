c0aa07eb891a05d929568031b14436fadf758dba
Levent Saatçi <leventsaatci@hotmail.com>
2018-07-28 12:30:03 +0200

    validations moved to QuerystringService

--- CoinMarketCap/Services/QueryStringService.cs
+++ CoinMarketCap/Services/QueryStringService.cs
@@ -1,18 +1,27 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 
 namespace CoinMarketCap.Services
 {
     public class QueryStringService
     {
-        public string AppendQueryString(string segment, params string[] parameters)
+        public static string AppendQueryString(string path, Dictionary<string, string> parameter)
         {
-            var encodedParams = parameters
+            var urlParameters = new List<string>();
+            foreach (var par in parameter)
+            {
+                urlParameters.Add(string.IsNullOrWhiteSpace(par.Value) ? null : $"{par.Key}={par.Value}");
+            }
+
+            var encodedParams = urlParameters
                 .Where(x => !string.IsNullOrWhiteSpace(x))
-                .Select(System.Net.WebUtility.HtmlEncode)
+                .Select(WebUtility.HtmlEncode)
                 .Select((x, i) => i > 0 ? $"&{x}" : $"?{x}")
                 .ToArray();
 
-            return encodedParams.Length > 0 ? $"{segment}{string.Join(string.Empty, encodedParams)}" : segment;
+            return encodedParams.Length > 0 ? $"{path}{string.Join(string.Empty, encodedParams)}" : path;
         }
     }
 }
